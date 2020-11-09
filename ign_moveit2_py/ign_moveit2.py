3d8a52f23783567e031171a5cc2d783918e38c57
Andrej Orsula <orsula.andrej@gmail.com>
2020-11-09 10:31:12 +0100

    Python interface - Add compute_ik()

--- ign_moveit2_py/ign_moveit2.py
+++ ign_moveit2_py/ign_moveit2.py
@@ -1,5 +1,5 @@
 """
-A simple python interface with MoveIt2 move_group action server for manipulation within Ignition Gazebo. The interface 
+A simple python interface with MoveIt2 move_group action server for manipulation within Ignition Gazebo. The interface
 with move_group action server is an alternative to moveit_commander, which is not yet ported to ROS 2 (as of Oct 2020).
 
 Note: This module is currently configured for Franka Emika Panda robot.
@@ -10,11 +10,16 @@ from rclpy.node import Node
 from rclpy.action import ActionClient
 
 from action_msgs.msg import GoalStatus
+from moveit_msgs.msg import Constraints, JointConstraint, PositionConstraint, OrientationConstraint, PositionIKRequest
+from moveit_msgs.action import MoveGroup
+from moveit_msgs.srv import GetPositionIK
+from trajectory_msgs.msg import JointTrajectory
 from geometry_msgs.msg import Pose, Quaternion
 from shape_msgs.msg import SolidPrimitive
-from trajectory_msgs.msg import JointTrajectory
-from moveit_msgs.msg import Constraints, JointConstraint, PositionConstraint, OrientationConstraint
-from moveit_msgs.action import MoveGroup
+from sensor_msgs.msg import JointState
+from builtin_interfaces.msg import Duration
+
+from typing import List
 
 
 class IgnitionMoveIt2Interface(Node):
@@ -25,11 +30,32 @@ class IgnitionMoveIt2Interface(Node):
         This also initialises request msg to improve performance for repreated requests.
         """
         super().__init__("ign_moveit2_py")
-        self.action_client_ = ActionClient(self, MoveGroup, "move_action")
-        self.publisher_ = self.create_publisher(JointTrajectory,
-                                                "joint_trajectory", 1)
+
+        # Subscriber of current joint positions
+        self.joint_state_sub_ = self.create_subscription(JointState,
+                                                         '/joint_states',
+                                                         self.joint_states_callback, 1)
+
+        # Service client for IK
+        self.compute_ik_client_ = self.create_client(GetPositionIK,
+                                                     "/compute_ik")
+        while not self.compute_ik_client_.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Compute IK service not available, waiting again...')
+
+        # Service client for path planning
+        self.move_action_client_ = ActionClient(self, MoveGroup,
+                                                "move_action")
+        while not self.move_action_client_.wait_for_server(timeout_sec=1.0):
+            self.get_logger().info('Move action server not available, waiting again...')
+
+        # Publisher of trajectories
+        self.joint_trajectory_pub_ = self.create_publisher(JointTrajectory,
+                                                           "joint_trajectory", 1)
+
+        # Initialise robot and all messages
         self.init_robot()
-        self.init_request_msg()
+        self.init_path_request_msg()
+        self.init_ik_request_msg()
         self.get_logger().info("MoveIt2 action client initialised successfuly")
 
     def init_robot(self):
@@ -62,9 +88,11 @@ class IgnitionMoveIt2Interface(Node):
         # self.gripper_joints_ = ["panda_finger_joint1",
         #                         "panda_finger_joint2"]
 
-    def init_request_msg(self):
+        self.current_joint_state_ = JointState()
+
+    def init_path_request_msg(self):
         """
-        Initialise default requst (goal) for `move_action` action.
+        Initialise default request (goal) for `move_action` action.
         """
         goal_msg = MoveGroup.Goal()
         goal_msg.request.group_name = self.arm_group_name_
@@ -111,21 +139,21 @@ class IgnitionMoveIt2Interface(Node):
         goal_msg.request.goal_constraints = [Constraints()]
 
         # Save message template to internals
-        self.goal_msg_ = goal_msg
+        self.path_goal_msg_ = goal_msg
 
     def clear_goal(self):
         """
         Clear all goal constraints that were previously set.
         Note that this function is called after each successful `plan_trajectory()`.
         """
-        self.goal_msg_.request.goal_constraints = [Constraints()]
+        self.path_goal_msg_.request.goal_constraints = [Constraints()]
 
     def create_new_goal_constraint(self):
         """
         Create a new set of goal contraints that will be set together with the request. Each subsequent setting of goals
         with `set_joint_goal()`, `set_pose_goal()` and others will be added under this newly created set of contraints.
         """
-        self.goal_msg_.request.goal_constraints.append(Constraints())
+        self.path_goal_msg_.request.goal_constraints.append(Constraints())
 
     def set_joint_goal(self, joint_positions, tolerance=0.001, weight=1.0, joint_names=None):
         """
@@ -143,7 +171,7 @@ class IgnitionMoveIt2Interface(Node):
             goal.tolerance_below = tolerance
             goal.weight = weight
 
-            self.goal_msg_.request.goal_constraints[-1].joint_constraints.append(
+            self.path_goal_msg_.request.goal_constraints[-1].joint_constraints.append(
                 goal)
 
     def set_position_goal(self, position, tolerance=0.001, weight=1.0, frame=None):
@@ -167,7 +195,7 @@ class IgnitionMoveIt2Interface(Node):
         goal.constraint_region.primitives[0].dimensions = [tolerance]
         goal.weight = weight
 
-        self.goal_msg_.request.goal_constraints[-1].position_constraints.append(
+        self.path_goal_msg_.request.goal_constraints[-1].position_constraints.append(
             goal)
 
     def set_orientation_goal(self, quaternion, tolerance=0.001, weight=1.0, frame=None):
@@ -189,7 +217,7 @@ class IgnitionMoveIt2Interface(Node):
         goal.absolute_z_axis_tolerance = tolerance
         goal.weight = weight
 
-        self.goal_msg_.request.goal_constraints[-1].orientation_constraints.append(
+        self.path_goal_msg_.request.goal_constraints[-1].orientation_constraints.append(
             goal)
 
     def set_pose_goal(self, position, quaternion,
@@ -206,27 +234,27 @@ class IgnitionMoveIt2Interface(Node):
 
     def plan_trajectory(self):
         """
-        Plan trajectory with previously set goals. The plan is automatically publised on success with `pub_joint_trajectory()`.
+        Plan trajectory with previously set goals. The plan is automatically publised on success with `pub_moveit_trajectory()`.
         TODO: Return response/feedback in a nice way.
         """
         # Stamp message with current time
         clock_time_now_msg = self._clock.now().to_msg()
-        self.goal_msg_.request.workspace_parameters.header.stamp = clock_time_now_msg
-        for contraints in self.goal_msg_.request.goal_constraints:
+        self.path_goal_msg_.request.workspace_parameters.header.stamp = clock_time_now_msg
+        for contraints in self.path_goal_msg_.request.goal_constraints:
             for position_constraint in contraints.position_constraints:
                 position_constraint.header.stamp = clock_time_now_msg
             for orientation_constraint in contraints.orientation_constraints:
                 orientation_constraint.header.stamp = clock_time_now_msg
 
         self.get_logger().info("Waiting for action server...")
-        self.action_client_.wait_for_server()
+        self.move_action_client_.wait_for_server()
 
         self.get_logger().info("Sending plan request...")
-        self._send_goal_future = self.action_client_.send_goal_async(self.goal_msg_,
-                                                                     feedback_callback=self.action_feedback_callback)
+        self.send_goal_future_ = self.move_action_client_.send_goal_async(self.path_goal_msg_,
+                                                                          feedback_callback=self.action_feedback_callback)
 
         # Register response callback
-        self._send_goal_future.add_done_callback(self.action_response_callback)
+        self.send_goal_future_.add_done_callback(self.action_response_callback)
 
     def action_feedback_callback(self, feedback):
         """
@@ -246,26 +274,69 @@ class IgnitionMoveIt2Interface(Node):
 
         # Register result callback
         self.get_logger().info("Planning accepted")
-        self._get_result_future = goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.action_result_callback)
+        self.get_result_future_ = goal_handle.get_result_async()
+        self.get_result_future_.add_done_callback(self.action_result_callback)
 
     def action_result_callback(self, future):
         """
-        Result callback from `move_action` server that publishes JointTrajectory on success with `pub_joint_trajectory()`.
+        Result callback from `move_action` server that publishes JointTrajectory on success with `pub_moveit_trajectory()`.
+        This function automatically clears all previously set goals with `clear_goal()`.
         """
         status = future.result().status
         if status == GoalStatus.STATUS_SUCCEEDED:
             self.get_logger().info("Planning successful, publishing JointTrajectory for execution...")
-            self.pub_joint_trajectory(
+            self.pub_moveit_trajectory(
                 future.result().result.planned_trajectory)
         else:
             self.get_logger().error(
                 "Planning failed with status: {0}".format(status))
+        self.clear_goal()
 
-    def pub_joint_trajectory(self, moveit_trajectory):
+    def pub_moveit_trajectory(self, moveit_trajectory):
+        """
+        Publish MoveIt trajectory, such that it can be executed by JointTrajectoryController.
+        """
+        self.joint_trajectory_pub_.publish(moveit_trajectory.joint_trajectory)
+
+    def pub_joint_trajectory(self, joint_trajectory):
         """
         Publish JointTrajectory, such that it can be executed by JointTrajectoryController.
-        This function also clears all previously set goals with `clear_goal()`.
         """
-        self.publisher_.publish(moveit_trajectory.joint_trajectory)
-        self.clear_goal()
+        self.joint_trajectory_pub_.publish(joint_trajectory)
+
+    def joint_states_callback(self, msg):
+        self.current_joint_state_ = msg
+
+    def init_ik_request_msg(self):
+        """
+        Initialise default request for compute_ik service.
+        """
+        request_msg = GetPositionIK.Request()
+        request_msg.ik_request.group_name = self.arm_group_name_
+        request_msg.ik_request.ik_link_names = self.arm_links_
+        request_msg.ik_request.ik_link_name = self.arm_end_effector_
+        request_msg.ik_request.pose_stamped.header.frame_id = self.arm_base_link_
+        request_msg.ik_request.robot_state.is_diff = False
+        request_msg.ik_request.avoid_collisions = True
+        request_msg.ik_request.timeout.sec = 0
+        request_msg.ik_request.timeout.nanosec = 0
+
+        # Save message template to internals
+        self.ik_request_msg_ = request_msg
+
+    def compute_ik(self, pose, initial_joint_state=None, constrains=None) -> JointState:
+        if initial_joint_state == None:
+            self.ik_request_msg_.ik_request.robot_state.joint_state = self.current_joint_state_
+        else:
+            self.ik_request_msg_.ik_request.robot_state.joint_state = initial_joint_state
+
+        if constrains != None:
+            self.ik_request_msg_.ik_request.constraints = constrains
+
+        self.ik_request_msg_.ik_request.pose_stamped.pose = pose
+        self.ik_request_msg_.ik_request.pose_stamped.header.stamp = self._clock.now().to_msg()
+
+        result = self.compute_ik_client_.call(self.ik_request_msg_)
+
+        # if result.
+        return result.solution.joint_state
